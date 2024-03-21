b8e41f81d4cb0ef6af5e9511975d3e8a382f6b3e
Chris Straw <chris.straw@outerlimitstech.com>
2024-03-21 07:00:34 -0400

    code coverage

--- tests/OLT.Extensions.EPPlus.Tests/ExcelWorkbookExtensionsTests.cs
+++ tests/OLT.Extensions.EPPlus.Tests/ExcelWorkbookExtensionsTests.cs
@@ -15,15 +15,6 @@ namespace OLT.Extensions.EPPlus.Tests
             ExcelWorkbook workbook1 = ExcelPackage2.Workbook;
             ExcelWorkbookStyleExtensions.CreateNamedStyle(workbook1, "Style1", act => act.SetFontAsBold());
 
-
-            //ExcelWorkbookExtensions.
-
-            //-----------------------------------------------------------------------------------------------------------
-            // Arrange
-            //-----------------------------------------------------------------------------------------------------------
-            ExcelWorksheet worksheet1 = ExcelPackage2.GetWorksheet("RandomOrderedColumns");
-
-
             //-----------------------------------------------------------------------------------------------------------
             // Act
             //-----------------------------------------------------------------------------------------------------------
@@ -34,7 +25,27 @@ namespace OLT.Extensions.EPPlus.Tests
             // Assert
             //-----------------------------------------------------------------------------------------------------------
             act1.Should().Throw<InvalidOperationException>().WithMessage("The Excel package already has a style with the name of 'Style1'");
-            //act2.Should().Throw<ExcelValidationException>().WithMessage("'Barcode' column is duplicated (rowIndex: 1)");
+        }
+
+
+        [Fact]
+        public void Should_check_named_style_found()
+        {
+
+            ExcelWorkbook workbook1 = ExcelPackage2.Workbook;
+            ExcelWorkbookStyleExtensions.CreateNamedStyle(workbook1, "Style1", act => act.SetFontAsBold());
+
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action act1 = () => ExcelWorkbookStyleExtensions.CreateNamedStyleIfNotExists(workbook1, "Style1", act => act.SetTextVertical());
+
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            act1.Should().NotThrow<InvalidOperationException>();
         }
     }
 }
