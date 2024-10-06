edaf0dc53c8c919cd70d318a6d959c17a2c8e718
Chris Straw <chris.straw@outerlimitstech.com>
2024-10-06 08:21:59 -0400

    Code Coverage

--- tests/OLT.Extensions.EPPlus.Tests/ExcelHelper_Tests.cs
+++ tests/OLT.Extensions.EPPlus.Tests/ExcelHelper_Tests.cs
@@ -34,5 +34,31 @@ namespace OLT.Extensions.EPPlus.Tests
                 .Throw<ArgumentOutOfRangeException>()
                 .WithMessage("Must only be 1-2 alpha characters (Parameter 'columnLetter')");
         }
+
+        [Theory]
+        [InlineData(1, "A")]
+        [InlineData(33, "AG")]
+        [InlineData(702, "ZZ")]
+        [InlineData(633, "XI")]
+        public void ColumnIndexToColumnLetter(int colIdx, string expected)
+        {
+            var result = ExcelHelper.ColumnIndexToColumnLetter(colIdx);
+            Assert.Equal(result, expected);
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(703)]
+        [InlineData(50000)]
+        public void ColumnIndexToColumnLetterInvalid(int colIdx)
+        {
+            Action action = () => ExcelHelper.ColumnIndexToColumnLetter(colIdx);
+            action.Should()
+                .Throw<ArgumentOutOfRangeException>()
+                .WithMessage("Must be between 1 and 702 (Parameter 'colIndex')");
+        }
+
+
     }
 }
